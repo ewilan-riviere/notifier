9006c2025f2ad8b08830316e40e36172b6699ef8
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-07-16 20:50:49 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Notifier
+
+[![go][go-version-src]][go-version-href]
+[![license][license-src]][license-href]
+
+[go-version-src]: https://img.shields.io/static/v1?style=flat-square&label=Go&message=v1.20&color=00ADD8&logo=go&logoColor=ffffff&labelColor=18181b
+[go-version-href]: https://go.dev/
+[license-src]: https://img.shields.io/github/license/kiwilan/php-rss.svg?style=flat-square&colorA=18181B&colorB=00ADD8
+[license-href]: https://github.com/kiwilan/php-rss/blob/main/README.md
+
+## Local
+
+```bash
+go get github.com/joho/godotenv
+```
+
+## Build
+
+```bash
+go build -o notifier
+./notifier hello
+```
+
+## Test
+
+Check with `curl` if the webhook is working.
+
+```bash
+curl -i -H "Accept: application/json" \
+  -H "Content-Type:application/json" \
+  -X POST --data \
+  "{\"content\": \"Posted Via Command line\"}" \
+  https://discord.com/api/webhooks/SERVER_ID/SERVER_TOKEN
+```
--- main.go
+++ main.go
@@ -1,26 +1,74 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"notifier/src/dotenv"
 	"os"
 )
 
 func main() {
-	// Check if any command-line arguments are provided
-	if len(os.Args) < 2 {
-		fmt.Println("Please provide a subcommand.")
+	fmt.Println("Welcome to Notifier!")
+	fmt.Println("Example: `notifier -name=default -service=discord 'hello !'`")
+
+	service := flag.String("service", "discord", "the service to send the message to: 'discord'|'slack'|'all'")
+	name := flag.String("name", "default", "the name of the server")
+
+	flag.Parse()
+
+	help()
+
+	if len(flag.Args()) < 1 {
+		fmt.Println("Please provide a message.")
 		os.Exit(1)
 	}
 
-	// Get the subcommand from the first command-line argument
-	subcommand := os.Args[1]
+	content := flag.Args()[0]
+	fmt.Println("Message:", content)
+	send(*name, *service, content)
+}
 
-	// Handle the subcommand
-	switch subcommand {
-	case "hello":
-		fmt.Println("Hello, World!")
-	default:
-		fmt.Printf("Unknown subcommand: %s\n", subcommand)
+func help() {
+	fmt.Println()
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] MESSAGE\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "Send a message to services: Discord, Slack")
+		fmt.Fprintln(os.Stderr, "")
+		fmt.Fprintln(os.Stderr, "Options:")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		flag.Usage()
 		os.Exit(1)
+	}
+}
+
+func send(name string, service string, content string) {
+	server := dotenv.Handle()
+
+	var sended bool = false
+	for i := 0; i < len(server.Items); i++ {
+		el := server.Items[i]
+
+		if string(el.Type) == service {
+			if el.Name == name {
+				el.Send(content)
+				sended = true
+			}
+		} else if service == "all" {
+			if el.Name == name {
+				el.Send(content)
+				sended = true
+			}
+		}
+	}
+
+	if !sended {
+		fmt.Println("Not found.")
+		fmt.Println("Name:", name)
+		fmt.Println("Service:", service)
 	}
 }
